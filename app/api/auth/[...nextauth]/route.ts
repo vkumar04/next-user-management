c8a29a89efba422e60a6545eaca3bcc249f99597
Vick Kumar <vick@tydei.io>
2023-10-23 09:56:07 -0400

    added authoptions type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,19 @@
-import nextAuth from "next-auth";
+import nextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import Credentials from "@auth/core/providers/credentials";
+import { GoogleProfile } from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
