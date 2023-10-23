8170f2a5ae6f9c77e3207665a8993d081bd1d50d
Vick Kumar <vick@tydei.io>
2023-10-23 10:00:07 -0400

    moved auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,5 @@
-import nextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { GoogleProfile } from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      profile(profile: GoogleProfile) {
-        return {
-          id: profile.id,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET ?? "",
-};
+import nextAuth from "next-auth";
+import { authOptions } from "@/util/authOptions";
 
 export const handler = nextAuth(authOptions);
 export { handler as GET, handler as POST };
