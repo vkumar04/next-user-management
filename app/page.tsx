793add3ff92d3e8429653f354665a389c242dcf1
Vick Kumar <vick@tydei.io>
2023-10-25 14:44:03 -0400

    added condition

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Home Page</h1>
-      <p className="text-4xl font-bold">{`Hello ${sessionData?.user?.name}! you are a ${sessionData?.user?.role}`}</p>
+      {session && (
+        <p className="text-4xl font-bold">{`Hello ${sessionData?.user?.name}! you are a ${sessionData?.user?.role}`}</p>
+      )}
     </main>
   );
 }
