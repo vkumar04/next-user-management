ccc6ef35bba06ee6a1e17d77cb6dd2978d868d97
Vick Kumar <vick@tydei.io>
2023-10-25 14:31:23 -0400

    removed redirect

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,17 +1,4 @@
-import { getServerSession } from "next-auth";
-
 export default async function admin() {
-  const session = await getServerSession();
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Admin Page</h1>
