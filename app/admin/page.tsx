32c6e247b358fd96b4732afa5307cf4f45c2c4a7
Vick Kumar <vick@tydei.io>
2023-10-25 21:42:41 -0400

    added json data to admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,11 @@
+import { getServerSession } from "next-auth";
+
 export default async function admin() {
+  const session = await getServerSession();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Admin Page</h1>
+      <pre>{JSON.stringify(session?.user, null, 2)}</pre>
     </main>
   );
 }
--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -3,6 +3,7 @@
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+
 export default function Navigation() {
   const { data: session } = useSession();
   return (
