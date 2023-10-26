af08fb507fb5437998b09761c99f9099a91dcd3a
Vick Kumar <vick@tydei.io>
2023-10-25 21:50:21 -0400

    got role displaying

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,8 @@
+import { authOptions } from "@/util/authOptions";
 import { getServerSession } from "next-auth";
 
 export default async function admin() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Admin Page</h1>
